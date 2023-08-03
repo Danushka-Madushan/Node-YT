e1657b78310ad41150e026d9ea57f254a88ca812
Danushka-Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2023-08-03 20:42:52 +0530

    Update yt.ts

--- src/core/scraper/yt.ts
+++ src/core/scraper/yt.ts
@@ -15,7 +15,23 @@ export const fetchYoutube = async (url: string): Promise<TRes> => {
     ),
     {
         headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.51'
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'DNT': '1',
+            'Origin': 'https://yt1s.com',
+            'Pragma': 'no-cache',
+            'Referer': 'https://yt1s.com/en607',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
+            'X-Requested-With': 'XMLHttpRequest',
+            'sec-ch-ua': '"Not/A)Brand";v="99", "Microsoft Edge";v="115", "Chromium";v="115"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"'
         }
     })
 
