7cd0acaa5335971e0964f5fdeebdf321dd53644a
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-08-05 19:14:44 +0530

    revert chanings

--- src/routes/base-route.ts
+++ src/routes/base-route.ts
@@ -151,45 +151,4 @@ app.get('/yt/convert/:id', async (req, res) => {
     })
 })
 
-app.get('/yt/yt1s/:id', async (req, res) => {
-    const { params: { id } } = req
-
-    const { body } = await got.post('https://yt1s.com/api/ajaxSearch/index', {
-        headers: {
-            'Accept': '*/*',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Cookie': '_ga_SHGNTSN7T4=GS1.1.1691242563.8.0.1691242563.0.0.0; _ga=GA1.2.1863704939.1687515344; _gid=GA1.2.1862366645.1691242564; _gat_gtag_UA_173445049_1=1',
-            'DNT': '1',
-            'Origin': 'https://yt1s.com',
-            'Pragma': 'no-cache',
-            'Referer': 'https://yt1s.com/en607',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
-            'X-Requested-With': 'XMLHttpRequest',
-            'sec-ch-ua': '"Not/A)Brand";v="99", "Microsoft Edge";v="115", "Chromium";v="115"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"'
-        },
-        form: {
-            'q': 'https://www.youtube.com/watch?v=fWQgWbpEjrE',
-            'vt': 'home'
-        }
-    })
-
-    if (!id) {
-        return ExpressResponse(res, false, 400, {
-            message: 'convert id required'
-        })
-    }
-
-    return ExpressResponse(res, true, 200, {
-        data: body
-    })
-})
-
 export default app
