2b3ef060b9f79cbb8a8af0e025ec31161c6ff2e6
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-08-07 11:12:47 +0530

    update base-route.ts

--- src/routes/base-route.ts
+++ src/routes/base-route.ts
@@ -6,8 +6,32 @@ import { TQuery, TStreamSession, TStreamsArray, ytResponse } from 'youtube/fetch
 import { fetchYoutube } from '../core/scraper/yt.js';
 import { generateMD5 } from '../core/utils/hash.js';
 import { findRecord, insertRecord } from '../core/redis/cache.js';
+import got from 'got';
 
-const VIDRegex = /^https?:\/\/w{3}[.]youtube[.]com\/watch\?v=(?<vid>.+)$/i
+const VIDRegex = /^https?:\/\/w{3}[.]youtube[.]com\/watch\?v=(?<vid>[A-z-_]+)/i
+
+const insertStreams = (
+    streams: Array<TStreamsArray>,
+    url: string,
+    mime: {
+        [key: string]: string;
+    } | undefined,
+    qualityLabel: string,
+    size: string,
+    quality: string,
+    lastModified: string,
+    convertRequired: boolean
+    ) => {
+    streams.push({
+        url: url,
+        mimeType: mime ? mime['quality'] : false,
+        qualityLabel: quality,
+        size: Math.round(parseInt(size) / 1024) / 1000 || false,
+        quality: qualityLabel,
+        lastModified: new Date(parseInt(lastModified)).toString(),
+        convertRequired: convertRequired
+    })
+}
 
 app.get('/yt/fetch', async (req: Request<object, object, object, TQuery>, res: Response) => {
     const { query: { url } } = req
@@ -27,7 +51,7 @@ app.get('/yt/fetch', async (req: Request<object, object, object, TQuery>, res: R
     }
 
     const data = await fetchYoutube(_id.groups['vid'])
-
+    console.log(JSON.stringify(data, null, 4))
     if (data && data.groups) {
         const streamSession: TStreamSession = {}
 
@@ -42,25 +66,9 @@ app.get('/yt/fetch', async (req: Request<object, object, object, TQuery>, res: R
             const mime = /^video\/(?<quality>[^;]+)/i.exec(mimeType)?.groups
 
             if (/^video/.test(mimeType)) {
-                streams.videos.push({
-                    url: url,
-                    mimeType: mime ? mime['quality'] : false,
-                    qualityLabel: quality,
-                    size: Math.round(parseInt(contentLength) / 1024) / 1000 || false,
-                    quality: qualityLabel,
-                    lastModified: new Date(parseInt(lastModified)).toString(),
-                    convertRequired: false
-                })
+                insertStreams(streams.videos, url, mime, quality, contentLength, qualityLabel, lastModified, false)
             } else {
-                streams.audios.push({
-                    url: url,
-                    mimeType: mime ? mime['quality'] : false,
-                    qualityLabel: quality,
-                    size: Math.round(parseInt(contentLength) / 1024) / 1000 || false,
-                    quality: qualityLabel,
-                    lastModified: new Date(parseInt(lastModified)).toString(),
-                    convertRequired: false
-                })
+                insertStreams(streams.audios, url, mime, quality, contentLength, qualityLabel, lastModified, false)
             }
         }
 
@@ -77,28 +85,12 @@ app.get('/yt/fetch', async (req: Request<object, object, object, TQuery>, res: R
             }
             
             if (/^video/.test(mimeType)) {
-                streams.videos.push({
-                    url: token,
-                    mimeType: mime ? mime['quality'] : false,
-                    qualityLabel: quality,
-                    size: Math.round(parseInt(contentLength) / 1024) / 1000 || false,
-                    quality: qualityLabel,
-                    lastModified: new Date(parseInt(lastModified)).toString(),
-                    convertRequired: true
-                })
+                insertStreams(streams.videos, url, mime, quality, contentLength, qualityLabel, lastModified, true)
                 streamSession[token] = {
                     video: url
                 }
             } else {
-                streams.audios.push({
-                    url: url,
-                    mimeType: mime ? mime['quality'] : false,
-                    qualityLabel: quality,
-                    size: Math.round(parseInt(contentLength) / 1024) / 1000 || false,
-                    quality: qualityLabel,
-                    lastModified: new Date(parseInt(lastModified)).toString(),
-                    convertRequired: false
-                })
+                insertStreams(streams.audios, url, mime, quality, contentLength, qualityLabel, lastModified, false)
             }
         }
         
@@ -148,6 +140,33 @@ app.get('/yt/convert/:id', async (req, res) => {
     return ExpressResponse(res, true, 200, {
         data: await findRecord(id)
     })
+})
+
+app.get('/yt/test', async (req, res) => {
+    const response = await got.get('https://loader.to/ajax/download.php', {
+        searchParams: {
+            'format': '1080',
+            'url': 'https://www.youtube.com/watch?v=bnVUHWCynig'
+        },
+        headers: {
+            'authority': 'loader.to',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'no-cache',
+            'dnt': '1',
+            'origin': 'https://en.loader.to',
+            'pragma': 'no-cache',
+            'referer': 'https://en.loader.to/',
+            'sec-ch-ua': '"Not/A)Brand";v="99", "Microsoft Edge";v="115", "Chromium";v="115"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188'
+        }
+    }).json<object>()
+    return res.status(200).json(response)
 })
 
 export default app
